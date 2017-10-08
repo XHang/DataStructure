f7b709658fda302f3f7f56e9c7d6cc3474c1b9c8
XiaoHang <1083594261@1-PC>
2017-10-08 17:16:02 +0800

    挖坑 多叉树

---
+++ src/test/java/com/cxh/binarytree/BinaryTreeTest.java
@@ -0,0 +1,32 @@
+package com.cxh.binarytree;
+
+import org.junit.Test;
+
+/**
+ * 二叉树的测试程序-创建一个简单的二叉树
+ * @author Administrator
+ *
+ */
+public class BinaryTreeTest {
+	@Test
+	public void createBinaryTreeTest(){
+		BinaryTree tree = new BinaryTree();
+		tree.rootNode = new Node("1");
+        /* 现在这颗树变成了这样
+		        1
+		      /   \
+		    null  null     
+         */
+		tree.rootNode.leftChildNode = new Node("2");
+		tree.rootNode.rightChildNode = new Node("3");
+		/* 进一步进化
+	        1
+	      /   \
+	     2      3
+	   /    \    /  \
+	 null null null null  
+	 */
+	tree.rootNode.leftChildNode.leftChildNode = new Node("4");
+	//现在的树就如readme文件所说的那棵树了
+	}
+}
