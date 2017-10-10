bfb9c750db500616ae1f880db8a9415db5ac2e45
XiaoHang <1083594261@1-PC>
2017-10-10 17:35:58 +0800

    二叉树遍历搞定 求深度待定

---
+++ src/main/java/com/cxh/binarytree/BinaryTreeTraversal.java
@@ -0,0 +1,67 @@
+package com.cxh.binarytree;
+
+
+/**
+ * 二叉树的遍历
+ * @author Administrator
+ *
+ */
+public class BinaryTreeTraversal {
+	
+	
+	public static BinaryTree tree;
+	/**
+	 * 中序遍历(递归)
+	 */
+	private void inorderTraversal(Node node){
+		if(node == null){
+			return ;
+		}
+		inorderTraversal(node.leftChildNode);
+		System.out.print(node.key+"    ");
+		inorderTraversal(node.rightChildNode);
+	}
+	
+	/**
+	 * 后序遍历(递归)
+	 */
+	private void postTraversal(Node node){
+		if(node == null){
+			return ;
+		}
+		postTraversal(node.leftChildNode);
+		postTraversal(node.rightChildNode);
+		System.out.print(node.key+"    ");
+	}
+	
+	
+	/**
+	 * 前序遍历(递归)
+	 * @param node
+	 */
+	private void preTraversal(Node node){
+		if(node == null){
+			return ;
+		}
+		System.out.print(node.key+"    ");
+		preTraversal(node.leftChildNode);
+		postTraversal(node.rightChildNode);
+	}
+	
+	
+	public void inorderTraversal(BinaryTree tree){
+		inorderTraversal(tree.rootNode);
+		System.out.println();
+	}
+	public void postTraversal(BinaryTree tree){
+		postTraversal(tree.rootNode);
+		System.out.println();
+	}
+	public void preTraversal(BinaryTree tree){
+		preTraversal(tree.rootNode);
+		System.out.println();
+	}
+	
+	
+
+}
