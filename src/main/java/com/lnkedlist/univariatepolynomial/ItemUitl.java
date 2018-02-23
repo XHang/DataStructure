ca6aa1c2e95737d28d3702bceb1530b1a285ba4e
XiaoHang <1083594261@1-PC>
2018-02-23 17:37:09 +0800

    一元多项式链表实现--加法完成 差冒泡排序使之降幂

--- src/main/java/com/lnkedlist/univariatepolynomial/ItemUitl.java
+++ src/main/java/com/lnkedlist/univariatepolynomial/ItemUitl.java
@@ -10,20 +10,23 @@ public class ItemUitl {
 	 * @return 多项式的第一项
 	 */
 	public static Item  addItem(Integer exp,Integer coef,Item item){
+		//如果第一项没有，则传进来的指数和系数作为多项式的第一项返回出去
 		if(item == null){
 			return new Item(exp, coef);
 		}
+		//循环为了获取多项式的最后一项
 		Item temp = item;
 		while(temp.nextItem!=null){
 			temp = temp.nextItem;
 		}
+		//多项式的最后一项的下一项赋值
 		temp.nextItem = new Item(exp, coef);
 		return item;
 	}
 	/**
 	 * 将一条链表附加在另一条链表的后面
 	 * 将一个多项式加在另一个多项式的后面
-	 * @param item 元多项式
+	 * @param item 原一元多项式
 	 * @param additionItem 需要附加的多项式
 	 * @return
 	 */
--- src/test/java/com/lnkedlist/application/UnivariatePolynomialAddCalculate.java
+++ src/test/java/com/lnkedlist/application/UnivariatePolynomialAddCalculate.java
@@ -3,21 +3,39 @@ package com.lnkedlist.application;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
+
+import org.junit.Test;
 
 import com.lnkedlist.univariatepolynomial.Item;
+import com.lnkedlist.univariatepolynomial.ItemComparator;
 import com.lnkedlist.univariatepolynomial.ItemUitl;
 
 /**
  * 链表应用，一元多项式的相加
- * @author Mr-hang
+ * @author 航
+ * 编写日期 ：2018/2/23
  *
  */
 public class UnivariatePolynomialAddCalculate {
 	
+	
+	/**
+	 * 用户输入两个多项式，打印出来，并计算其相加后的结果
+	 * 请用Junit运行
+	 * @throws IOException
+	 */
+	@Test
 	public void addCalculate() throws  IOException{
+		System.out.println("请输入第一条多项式");
 		Item item1 = getItem();
+		printlnUnivariatePolynomial(item1);
+		System.out.println("请输入第二条多项式");
 		Item item2 = getItem();
-		
+		printlnUnivariatePolynomial(item2);
+		System.out.println("计算多项式的和");
+		Item merageItem = addCalculate(item1,item2);
+		printlnUnivariatePolynomial(merageItem);
 	}
 	
 	/**
@@ -64,15 +82,15 @@ public class UnivariatePolynomialAddCalculate {
 			}
 			//如果两个项相加后系数为0，消除，无需添加项
 			if(coef !=0){
-				ItemUitl.addItem(exp, coef, mergeItem);
+				mergeItem = ItemUitl.addItem(exp, coef, mergeItem);
 			}
 		}
 		//如果比较后，其中一个多项式中的项已经全部比较完，但是另一个多项式还有余项
 		//就把余项附加到合并项里面
 		if(item1 != null){
-			ItemUitl.addItem(mergeItem, item1);
+			mergeItem = ItemUitl.addItem(mergeItem, item1);
 		}else if(item2 !=null){
-			ItemUitl.addItem(mergeItem, item1);
+			mergeItem = ItemUitl.addItem(mergeItem, item2);
 		}
 		return mergeItem;
 	}
@@ -90,20 +108,98 @@ public class UnivariatePolynomialAddCalculate {
 		Item item = null;
 		while(true){
 			try {
-				System.out.println("请输入第"+count+"项的指数");
-				Integer exp = new Integer(reader.readLine());
 				System.out.println("请输入第"+count+"项的系数");
 				Integer coef = new Integer(reader.readLine());
+				System.out.println("请输入第"+count+"项的指数");
+				Integer exp = new Integer(reader.readLine());
 				item = ItemUitl.addItem(exp, coef, item);
 			} catch (NumberFormatException e) {
 				System.err.println("请输入正确的的数字格式");
 				continue;
 			}
 			System.out.println("已添加一项，键入回车继续添加，输入exit退出多项式的建立");
+			count++;
 			if("exit".equals(reader.readLine())){
 				break;
 			}
 		}
 		return item;
+	}
+	
+	/**
+	 * 打印一条多项式
+	 * @param item 多项式的第一项
+	 * TODO 如果其中一项是负数的话
+	 */
+	private void printlnUnivariatePolynomial(Item item){
+		Integer coef = null;
+		Integer exp = null;
+		while(item!=null){
+			coef = item.coef;
+			exp = item.exp;
+			//打印系数(系数为1，无需打印系数)
+			if(coef != 1){
+				System.out.print(coef);
+			}
+			//如果指数为0，则无需打印未知数和指数
+			if(exp != 0){
+				//如果指数为1，无需打印指数，只需打印未知数即可
+				if(exp == 1){
+					System.out.print("x");
+				}else{
+					System.out.print("x^"+exp);
+				}
+			}
+			//如果最后项是空的话，不需要打印加号了
+			if(item.nextItem!=null){
+				System.out.print("+");
+			}
+			item = item.nextItem;
+		}
+		//多项式打印完后换行
+		System.out.println("");
+	}
+
+	/**
+	 * 对一条多项式进行降幂排列
+	 * @param item 多项式的第一项
+	 * @return 降幂后多项式的第一项
+	 * power by Google & Baidu
+	 * 本次要求冒泡排序不能通过链表的长度进行循环
+	 */
+	private Item sort(Item item){
+		//定义两个临时变量用于存储外部和内部循环的项
+		Comparator<Item> comparator = new ItemComparator(); 
+		Item outertemp = item;
+		Item innertemp = item;
+		//定义此变量用于存储内部循环遍历的前一项--用于交换
+		Item innerPre = null;
+		//每外部循环一次，outertemp就指向下一个项，如果为空，说明外部循环已经结束
+		//这样就不用去计算链表的长度来控制外内循环
+		while(outertemp !=null){
+			while(innertemp.nextItem !=null){
+				if(comparator.compare(innertemp, innertemp.nextItem)<0 ){
+					swap(innertemp,innertemp.nextItem,innerPre);
+				}else{
+					innerPre = innertemp;
+					innertemp = innertemp.nextItem;
+				}
+			}
+			outertemp = outertemp.nextItem;
+			//每次内部循环结束后，开启下一次循环都要置innerPre为null和innertemp为，以便从头开始排序
+			innerPre = null;
+			
+				
+		}
+		return item;
+	}
+	/**
+	 * 交换两个相邻项的位置
+	 * @param innertemp 前项
+	 * @param nextItem 后项
+	 * @param innerPre 前前项 可能为空，如果为空，代表前项乃第一项
+	 */
+	private void swap(Item innertemp, Item nextItem, Item innerPre) {
+		
 	}
 }
