1201303695bc5566ef3fcf7749cd95512c651e3c
XiaoHang <1083594261@1-PC>
2018-03-05 17:25:32 +0800

    栈对象添加几个常见方法 jdk栈对象的测试类-添加

--- src/main/java/com/stack/Stack.java
+++ src/main/java/com/stack/Stack.java
@@ -1,6 +1,6 @@
 package com.stack;
 
-import java.nio.channels.IllegalChannelGroupException;
+import java.util.EmptyStackException;
 
 /**
  * 一个栈对象
@@ -42,7 +42,7 @@ public class Stack<T> {
 		return top==-1;
 	}
 	
-	/*
+	/**
 	 * 取出栈顶元素
 	 */
     @SuppressWarnings("unchecked")
@@ -50,9 +50,16 @@ public class Stack<T> {
 		if(isEmpty()) {
 			throw new IllegalStateException("空栈取不出元素哦");
 		}
-		return (T)items[top--];
+		T t =  (T)items[top];
+		removeElementAt(top);
+		top--;
+		return t;
 	}
     
+    /**
+     * 压栈，将给定元素压到栈顶
+     * @param item
+     */
     public void set(T item) {
     	if(isFull()) {
     		throw new IllegalStateException("已经栈满,不允许再添加了哦");
@@ -60,4 +67,58 @@ public class Stack<T> {
     	//top指向栈顶元素，如果要继续压栈，则必须先让栈顶指针移动一位，然后再压栈
     	items[++top] = item;
     }
+    
+    /**
+     * 返回栈内元素数量
+     * @return
+     */
+    public int size(){
+    	//top指针从0开始，故计算容量要加1
+    	//因为容量是从1开始算的
+    	return top+1;
+    }
+    /**
+     * 根据角标移除数组中元素
+     * @param index
+     */
+    private void removeElementAt(int index){
+    	items[index] = null;
+    }
+    /**
+     * 窥视栈顶元素，而不从栈内删除
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+	public T peek(){
+    	if(isEmpty()){
+    		throw new EmptyStackException();
+    	}
+    	return (T)items[top];
+    }
+    
+    /**
+     * 搜索栈内元素是否有给定元素
+     * 使用对象的equals进行判定
+     * @param t
+     * @return 元素的位置，从栈顶指针开始算起，如果找不到这个元素，返回-1
+     */
+    public int search(T t){
+    	//判定为空是必须的，因为你不知道栈和参数，哪个会先为空
+    	if(t==null){
+    		for(int i=top;i >= 0;i--){
+    			if(items[top] == null){
+    				//之所以返回的值是栈内容量大小-i,之所以这么设计，因为元素的位置是从栈顶指针开始算起
+    				return size()-i;
+    			}
+    		}
+    	}else{
+    		for(int i=top;i >= 0;i--){
+    			if(t.equals(items[i])){
+    				return size()-i;
+    			}
+    		}
+    	}
+		return -1;
+    }
+    
 }
--- src/test/java/com/stack/JDKStackTest.java
+++ src/test/java/com/stack/JDKStackTest.java
@@ -1,33 +1,31 @@
 package com.stack;
 
+import java.util.Stack;
 
 import org.junit.Test;
 
 import com.stack.vo.User;
 
 /**
- * 栈测试
+ * JDK栈对象测试
  * @author Administrator
  *
  */
-public class StackTest {
+public class JDKStackTest {
 	
-	/**
-	 * 演示栈的初始化，添加，取出
-	 */
 	@Test
 	public void stackTest(){
-		//初始化栈，并指定了栈的大小
-		Stack<User> stack = new Stack<>(3);
-		//依次按0,1,2的顺序压栈
-		for(int i=0;i<3;i++){
+		System.out.println("下面演示的是jdk内部实现的栈对象");
+		Stack<User> stack = new Stack<>();
+		//依次按0,1,2,3的顺序压栈
+		for(int i=0;i<4;i++){
 			User user = new User();
 			user.setUsername("人员"+i);
 			user.setPassword("passwd"+i);
-			stack.set(user);
+			stack.push(user);
 		}
-		for(int i=0;i<3;i++){
-			User user = stack.get();
+		for(int i=0;i<4;i++){
+			User user = stack.pop();
 			System.out.println("栈内取的元素是"+user);
 		}
 	}
@@ -39,7 +37,7 @@ public class StackTest {
 	public void searchTest(){
 		//保存角标元素1的引用，以便在下面的搜索代码中用它来搜索
 		User user1 = null;
-		Stack<User> stack = new Stack<User>(20);
+		Stack<User> stack = new Stack<>();
 		//依次按0,1,2,3的顺序压栈
 		for(int i=0;i<4;i++){
 			User user = new User();
@@ -48,10 +46,15 @@ public class StackTest {
 			if(i==1){
 				user1 = user;
 			}
-			stack.set(user);
+			stack.push(user);
 		}
 		//搜索的是第二次压栈的元素，总共压栈四次，从上面数起，应该是第三位
 		System.out.println("search ing ....."+stack.search(user1));
+	}
+	
+	@Test
+	public void traverse(){
+		
 	}
 
 }
